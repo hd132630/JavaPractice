c56d1e57407e7fb4f558bbd2faa12ef27f17df4e
hd132630 <whwjdgus0@naver.com>
2015-08-20 12:00:35 +0900

    첫번째 실습과제 제출

---
+++ JavaPractice/src/org/dimigo/inheritance/Figure.java
@@ -0,0 +1,31 @@
+package org.dimigo.inheritance;
+
+public class Figure {
+	private int centerX;
+	private int centerY;
+	
+	public Figure() {
+		
+	}
+	
+	public Figure(int centerX, int centerY){
+		this.centerX = centerX;
+		this.centerY = centerY;
+	}
+	
+	protected double calcArea(){
+		return 0;
+	}
+	
+	protected void printCenter(){
+		System.out.println("중심좌표("+centerX+","+centerY+")");
+	}
+	
+	protected void moveCenter(int x, int y){
+		centerX += x;
+		centerY += y;
+		
+		
+	}
+}
+
---
+++ JavaPractice/src/org/dimigo/inheritance/FigureTest.java
@@ -0,0 +1,30 @@
+package org.dimigo.inheritance;
+
+public class FigureTest {
+
+	public static void main(String[] args) {
+		Circle c = new Circle(5);
+		Triangle t = new Triangle(10, 10, 5, 8);
+		Rectangle r = new Rectangle(20, 20, 5, 8);
+		
+		System.out.println(String.format("%.1f", c.calcArea()));
+		System.out.println(String.format("%.1f", t.calcArea()));
+		System.out.println(String.format("%.1f",r.calcArea()));
+
+		c.printCenter(); 
+		t.printCenter();
+		r.printCenter();
+		 
+		System.out.println("");
+		System.out.println("--중심좌표 이동(x,y축 5씩)");
+		System.out.println("");
+		c.moveCenter(5, 5);
+		t.moveCenter(5, 5);
+		r.moveCenter(5, 5);
+		
+		c.printCenter();
+		t.printCenter();
+		r.printCenter();
+	}
+
+}
