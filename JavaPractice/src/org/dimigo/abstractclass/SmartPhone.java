2eacf2ed2f12d3b1285ffaeb22dfcc337f4c2578
hd132630 <whwjdgus0@naver.com>
2015-09-03 11:13:13 +0900

    세번째 제출

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhone.java
@@ -0,0 +1,45 @@
+package org.dimigo.abstractclass;
+
+public abstract class SmartPhone {
+	private String model;
+	private String company;
+	private int price;
+	
+	public SmartPhone(){
+		
+	}
+	
+	public SmartPhone(String model, String company, int price) {
+		this.model = model;
+		this.company = company;
+		this.price = price;
+	}	
+	
+	public void turnOn(){
+		System.out.println(model + "의 전원을 켭니다.");
+	}
+	
+	public void turnOff(){
+		System.out.println(model + "의 전원을 끕니다.");
+	}
+	
+	public abstract void pay();
+	
+	public void useSpecialFunction(SmartPhone phone){
+		if(phone instanceof IPhone)
+		{
+			phone = (IPhone) phone;
+			((IPhone) phone).useAirDropFunction();  
+		}
+		if(phone instanceof Galaxy)
+		{
+			phone = (Galaxy) phone;
+			((Galaxy) phone).useWirelessCharging();
+		}
+		}
+	
+	public String toString(){
+		return "모델명 : " + model + ", 제조사 : " + company + ", 가격 : " + String.format("%,d", price) + "원";
+	}
+	
+}
